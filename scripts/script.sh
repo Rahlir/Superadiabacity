b3bf3d50d12af19426726d124cb0e24aa004f321
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2019-03-26 19:13:22 +0100

    Modify script

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,5 +4,5 @@ filename=$3
 matsc=$4
 
 awk -v fn="$filename" -v fm="$1" -v md="$2" -f scripts/change.awk brute_force/generate_pulse.m > brute_force/"$matsc".m &&
-matlab -nodisplay -nojvm -nosplash -nodesktop -r "cd brute_force;${matsc};exit" &&
+    matlab -nodisplay -nojvm -nosplash -nodesktop -r "cd('brute_force');${matsc};exit" &&
 echo DONE: $filename $matsc.m
