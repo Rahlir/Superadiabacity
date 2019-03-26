9709bb6fa51a79bfc46d6cde5437861549b0cde7
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2019-03-26 18:43:45 +0100

    Force delete of Scripts to be able to rename them

--- Scripts/parent_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-base=$(date "+%H_%M_%S")
-matsc=$(date "+opt%H%M%S")
-filename="$base".mat
-scriptname="$base".m
-
-ssh "$1".cs.dartmouth.edu "tmux send-keys -t MAIN.0 './script.sh "$2" "$3" "$4" "$filename" "$matsc" "$scriptname"' ENTER" &&
-echo RUNNING ON $1
-echo "scp $1.cs.dartmouth.edu:~/Development/Physics/Research/Code/NumericalExploration/$filename ." >> runafter.sh
