d791f14b740de1d7321991bc6be5bae24f8ae228
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2019-03-31 15:10:06 +0200

    Rename files

--- common/generate_random_pulse.m
+++ common/generate_random_pulse.m
@@ -21,5 +21,6 @@ function[delta_guess, omega_guess] = generate_random_pulse(nop, w1_max)
     omega_guess = omega_guess * w1_max / max(omega_guess);
 
     % choose ramp to maximize q3
+    % A = 1408010;
     A = rand * 19e4 + 1e4;
     delta_guess = - A : 2 * A / (nop - 1) : A;
