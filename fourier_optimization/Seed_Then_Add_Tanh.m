29b3bc59357b9c5910989131be58c53f8ec5af8e
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2019-03-31 16:30:12 +0200

    Format fix

--- fourier_optimization/Seed_Then_Add_Tanh.m
+++ fourier_optimization/Seed_Then_Add_Tanh.m
@@ -1,6 +1,6 @@
 % ------------------------------------------------------------------------------
-% script Seed_Then_Add_Tanh - 
-% 
+% script Seed_Then_Add_Tanh -
+%
 % Tadeáš Uhlíř
 % 11/16/2018
 % Original by:
@@ -24,7 +24,8 @@ omega_1 = prepare_initial_pulse(nop, power, detune_factor);
 delta_omega = get_tanh(nop, 25, 40, power, 15, T/nop);
 detune_factor = max(delta_omega); % set max detuning of initial LZ pulse
 initial_Q = get_Q_curves(delta_omega, omega_1, T/nop, 10);
-fprintf('Initial Q-factor: %.4f\n', initial_Q) 
+fprintf('Initial Q-factor: %.4f\n', initial_Q)
+
 % Prepare optimization.
 mode_bound = upperb_fac * detune_factor;    % restricts size of modes
 lowb = [1, -mode_bound * ones(1, mode_size)];   % set lower bounds
@@ -35,15 +36,17 @@ store_Q = zeros(frame_no, rep);
 store_Four = zeros(frame_no, rep, mode_size + mode_add + 1);
 store_pulses = zeros(frame_no, rep, nop);
 max_q = 0;
+
 for j = 1 : frame_no
     frame = j;
     fprintf('---FRAME %d---\n', frame)
     k = 0;
+
     for p = 1 : rep
         % Choose random seed.
         rand_guess = [1 + upperb_fac * rand, mode_bound * ...
             (rand(1, mode_size) * 2 - 1)];
-        
+
         % Optimize.
         options = optimoptions('fmincon','MaxFunEvals',1e7,'Display','notify');
         [par, fval] = fmincon(@(par) fourier_q(par, delta_omega, ...
@@ -51,13 +54,13 @@ for j = 1 : frame_no
             lowb, upperb, [], options);
 
         Q = 1/fval;
-        
+
         % If above threshold value, add modes.
         if Q > 0.8
             k = k + 1;
-            
+
             new_seed = [par, zeros(1, mode_add)];
-            
+
             [par, fval] = fmincon(@(par) fourier_q(par, delta_omega, ...
                 omega_1, T, frame), new_seed, [], [], [], [], ...
                 [lowb, -mode_bound * ones(1, mode_add)], ...
@@ -66,11 +69,11 @@ for j = 1 : frame_no
 
             Q_add = 1/fval;
             fprintf('Iteration: %d\n', p)
-            fprintf('Q-factor: %.4f\n', Q_add) 
-            
+            fprintf('Q-factor: %.4f\n', Q_add)
+
             % Store the results of the optimization.
             store_Q(j, k) = Q_add;
-            store_Four(j, k, :) = par;          
+            store_Four(j, k, :) = par;
             mod_delta = get_four_func(delta_omega, par);
             store_pulses(j, k, :) = mod_delta;
             if Q_add > max_q
